215f9b991e741c840bc36d9c957bfb3debbc4cc0
Catrina Mihela <catrina.mihaela20@gmail.com>
2019-03-23 15:38:30 +0200

    Add classes in python

--- api/app.py
+++ api/app.py
@@ -1,17 +1,19 @@
-from peewee import CharField, Model, MySQLDatabase
+from peewee import CharField, IntegerField, FloatField, BooleanField, ForeignKeyField, Model, MySQLDatabase
 from flask import Flask
+from flask import request, jsonify
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 MYSQL_DB = 'app'
-MYSQL_HOST = 'db'
-MYSQL_PORT = 3306
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = 6000
 MYSQL_USER = 'root'
 MYSQL_PASS = 'root'
 
 db = MySQLDatabase(MYSQL_DB, host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASS)
 db.connect()
+
 
 class BaseModel(Model):
     class Meta:
@@ -19,16 +21,53 @@ class BaseModel(Model):
 
 
 class Users(BaseModel):
+    email = CharField()
     username = CharField()
-    password = CharField()
+
+
+class Restaurants(BaseModel):
+    name = CharField()
+
+
+class Tables(BaseModel):
+    id_restaurant = ForeignKeyField(Restaurants, backref='table')
+    isOcupied = BooleanField()
+    status = IntegerField()
+    identify = IntegerField()
+
+
+class Products(BaseModel):
+    id_restaurant = ForeignKeyField(Restaurants, backref='product')
+    name = CharField()
+    price = FloatField()
+    ingredients = CharField()
+    picture = CharField()
+    category = CharField()
+
+
+class Orders(BaseModel):
+    id_table = ForeignKeyField(Tables, backref='Command')
+    id_user = ForeignKeyField(Users, backref='Command')
+    status = IntegerField()
+
+
+class Order_Products(BaseModel):
+    id_order = IntegerField()
+    id_product = IntegerField()
+
 
 
 @app.route('/')
 def hello_world():
-    user = Users.get_or_none(Users.username == 'Cosmin')
+    user = Users.get_or_none(Users.username == 'Ioana')
     return user.password
 
-    # return 'Hello World!'
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
